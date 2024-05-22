d4dc0efe9b695b0cab1df575d058f27488da3852
Sadie Sorceress <lomiskiam@gmail.com>
2024-05-22 10:00:19 -0600

    a

--- home.scm
+++ home.scm
@@ -11,6 +11,7 @@
              (gnu home services)
              (gnu home services dotfiles)
              (gnu home services symlink-manager)
+             (gnu home services ssh)
              (gnu home services shells))
 
 (define (service-dotfile . rest)
@@ -27,28 +28,34 @@
   ;; services, run 'guix home search KEYWORD' in a terminal.
   (services
     (list
-      #; (service home-symlink-manager-service-type)
-      #; (service home-dotfiles-service-type
-               (home-dotfiles-configuration
-                 (directories '("./dots"))
-                 (layout 'stow)))
-      #; (service home-zsh-service-type
-               (home-zsh-configuration
-                 (zshrc
-                   (list (local-file (service-dotfile "/zsh/zshrc"))
-                         (local-file (service-dotfile "/zsh/zsh-aliases"))
-                         (local-file (service-dotfile "/zsh/zsh-vi-mode/zsh-vi-mode.plugin.zsh"))))
-                 (zlogout
-                   (list (local-file (service-dotfile "/zsh/zsh-logout"))))
-                 (zshenv
-                   (list (local-file (service-dotfile "/zsh/zshenv"))))))
-      #; (service home-inputrc-service-type
-               (home-inputrc-configuration
-                 (variables
-                   `(("bell-style" . "none")
-                     ("colored-completion-prefix" . #t)
-                     ("editing-mode" . "vi")
-                     ("show-mode-in-prompt" . #t)
-                     ("completion-ignore-case" . #t)
-                     ("blink-matching-paren" . #t))))))))
+      ; (service home-symlink-manager-service-type)
+      ; (service home-dotfiles-service-type
+      ;          (home-dotfiles-configuration
+      ;            (directories '("./dots"))
+      ;            (layout 'stow)))
+      ; (service home-zsh-service-type
+      ;          (home-zsh-configuration
+      ;            (zshrc
+      ;              (list (local-file (service-dotfile "/zsh/zshrc"))
+      ;                    (local-file (service-dotfile "/zsh/zsh-aliases"))
+      ;                    (local-file (service-dotfile "/zsh/zsh-vi-mode/zsh-vi-mode.plugin.zsh"))))
+      ;            (zlogout
+      ;              (list (local-file (service-dotfile "/zsh/zsh-logout"))))
+      ;            (zshenv
+      ;              (list (local-file (service-dotfile "/zsh/zshenv"))))))
+      ; (service home-inputrc-service-type
+      ;          (home-inputrc-configuration
+      ;            (variables
+      ;              `(("bell-style" . "none")
+      ;                ("colored-completion-prefix" . #t)
+      ;                ("editing-mode" . "vi")
+      ;                ("show-mode-in-prompt" . #t)
+      ;                ("completion-ignore-case" . #t)
+      ;                ("blink-matching-paren" . #t)))))
+      (service home-openssh-service-type
+               (home-openssh-configuration
+                 #; (hosts
+                   (list (openssh-host (name "")
+                                       (user "charlie"))))
+                 (authorized-keys (list (local-file "keys/fruitbook.pub"))))))))
 
